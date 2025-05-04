07a4d009478e06c215fb15300bd2d4128fa35619
Ilya Lukanchev <bkmz432111@yandex.ru>
2025-05-04 16:01:41 +0400

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import org.hibernate.sql.Insert;
+
+import java.sql.*;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/test";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return connection;
+    }
 }
+
