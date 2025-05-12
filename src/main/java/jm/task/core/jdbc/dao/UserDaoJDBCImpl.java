e72bc98cf728b6641526349f90cdb58e458c441c
Ilya Lukanchev <bkmz432111@yandex.ru>
2025-05-12 20:43:42 +0400

    115 (1)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,10 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -14,77 +11,26 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            String sqlCommand = "CREATE TABLE IF NOT EXISTS Users (IdUsers INT PRIMARY KEY AUTO_INCREMENT," +
-                    " name VARCHAR(100), lastName VARCHAR(100), age INT)";
-            statement.executeUpdate(sqlCommand);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            String sqlCommand = "DROP TABLE IF EXISTS Users";
-            statement.executeUpdate(sqlCommand);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String insertSQL = "INSERT INTO Users (name, lastName, age) VALUES (?, ?, ?)";
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(insertSQL)) {
-            statement.setString(1, name);
-            statement.setString(2, lastName);
-            statement.setByte(3, age);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 
     public void removeUserById(long id) {
-        String removeSQL = "DELETE FROM Users WHERE IdUsers = ?";
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(removeSQL)) {
-            statement.setLong(1, id);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 
     public List<User> getAllUsers() {
-        ArrayList<User> allUsers = new ArrayList<>();
-        String getAllUsers = "SELECT * FROM Users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet result = statement.executeQuery(getAllUsers)) {
-            while (result.next()) {
-                User user = new User();
-                user.setId(result.getLong(1));
-                user.setName(result.getString(2));
-                user.setLastName(result.getString(3));
-                user.setAge(result.getByte(4));
-                allUsers.add(user);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return allUsers;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String clear = "TRUNCATE TABLE Users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.executeUpdate(clear);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,23 +1,45 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/test";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "admin";
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static SessionFactory sessionFactory;
+    //private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    public static Connection getConnection() {
-        Connection connection;
+    public static SessionFactory getConnection() {
         try {
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException | ClassNotFoundException ex) {
-            throw new RuntimeException(ex);
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://127.0.0.1:3306/test");
+            settings.put(Environment.USER, "admin");
+            settings.put(Environment.PASS, "admin");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            configuration.setProperties(settings);
+
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return connection;
+        return sessionFactory;
+        }
+
     }
-}
+
 
